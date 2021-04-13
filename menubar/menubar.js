cd1ce45a5cc3274756075a56bb970f2b97c70a66
Tangnnr <61136644+Tangnnr@users.noreply.github.com>
2021-04-13 13:49:17 +0700

    menubar

---
+++ menubar/menubar.js
@@ -0,0 +1,9 @@
+const toggleButton  = document.getElementsByClassName('toggle-button')[0]
+const nav = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('active');
+})
+
+
+
