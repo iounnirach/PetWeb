b68d8c13d11748dd9f30f4ec5bec5fa1a39ae407
IounOm <iounnirach15@gmail.com>
2021-05-25 18:47:30 +0700

    edit home.html/.css & add google api & create server.js
    add google map api in home.js / create server.js / edit home.html and home.css

--- home.js
+++ home.js
@@ -7,6 +7,8 @@ function pageload() {
     toggleButton.addEventListener('click', () => {
         nav.classList.toggle('active');
     })
+
+    initMap();
    
 }
 function ShowInfo() {
@@ -14,4 +16,133 @@ function ShowInfo() {
 }
 function CloseInfo() {
     document.getElementById("bgShow").style.display = "none";
+}
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: {lat: 13.847860, lng: 100.604274},
+    //   center: { lat: 34.84555, lng: -111.8035 },
+      mapTypeId: "roadmap",
+    });
+    // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+    // receives the initial focus when tab is pressed. Use arrow keys to
+    // move between markers; press tab again to cycle through the map controls.
+
+    var locations = [
+        [{ lat: 13.846876, lng: 100.604481}, 'วัดลาดปลาเค้า'],
+        [{ lat: 13.847766, lng: 100.605768}, 'หมู่บ้านอารียา'],
+        [{ lat: 13.845235, lng: 100.602711}, 'สปีดเวย์'],
+        [{ lat: 13.862970, lng: 100.613834}, 'สเต็ก ลุงหนวด']
+        ];
+    // const locations = [
+    //   [{ lat: 34.8791806, lng: -111.8265049 }, "Boynton Pass"],
+    //   [{ lat: 34.8559195, lng: -111.7988186 }, "Airport Mesa"],
+    //   [{ lat: 34.832149, lng: -111.7695277 }, "Chapel of the Holy Cross"],
+    //   [{ lat: 34.823736, lng: -111.8001857 }, "Red Rock Crossing"],
+    //   [{ lat: 34.800326, lng: -111.7665047 }, "Bell Rock"],
+    // ];
+    // Create an info window to share between markers.
+    const infoWindow = new google.maps.InfoWindow();
+    // Create the markers.
+    locations.forEach(([position, title], i) => {
+      const marker = new google.maps.Marker({
+        position,
+        map,
+        title: `${i + 1}. ${title}`,
+        label: `${i + 1}`,
+        optimized: false,
+      });
+      // Add a click listener for each marker, and set up the info window.
+      marker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent(marker.getTitle());
+        infoWindow.open(marker.getMap(), marker);
+      });
+    });
+  }
+
+// async function getDataReview(){
+// 	const response = await fetch("\getDBReview");
+// 	const content = await response.json();
+// 	showReview(content);
+// }
+
+// function showReview(data){
+// 	var showIDlayer = document.getElementById("reviewInfo");
+//     var keys = Object.keys(data);
+//     for(var i = 0; i < keys.length; i++){
+//         var container = document.createElement("div");
+//         container.className = "Review";
+//         var containerItem = document.createElement("div");
+//         containerItem.className = "bg-light pt-5 pt-lg-0 pt-xl-5 pb-5";
+//         var containerDetail = document.createElement("div");
+//         containerDetail.className = "mt-4 p-4 text-start";
+//         var containerButton = document.createElement("div");
+//         containerButton.className = "d-grid px-4";
+
+//         var furniture_pic = document.createElement("img");
+//         furniture_pic.className = "img-fluid";
+//         var furniture_name = document.createElement("h2");
+//         furniture_name.className = "pb-2 boldText";
+//         var size = document.createElement("p");
+//         var wood = document.createElement("p");
+//         var price = document.createElement("h3");
+//         price.className = "boldText";
+//         var detailLable = document.createElement("p");
+//         detailLable.className = "mb-0 pb-0";
+//         var detail = document.createElement("p");
+
+//         var cartBtn = document.createElement("button");
+//         cartBtn.className = "btn btn-orange";
+//         cartBtn.id = [data[keys[i]].FID];
+//         // cartBtn.onclick = getToCart(this.id);
+//         // cartBtn.attachEvent('OnClick',getToCart(this.id));
+
+//         furniture_pic.src = "furniturePic/" + data[keys[i]].furniture_pic;
+//         furniture_name.innerHTML = data[keys[i]].furniture_name;
+//         size.innerHTML = "ขนาด : " + data[keys[i]].size + " ซม.";
+//         wood.innerHTML = "เนื้อไม้ : " + data[keys[i]].wood;
+//         price.innerHTML = data[keys[i]].price + " บาท";
+//         detailLable.innerHTML = "รายละเอียดเพิ่มเติม :";
+//         detail.innerHTML = data[keys[i]].detail;
+//         cartBtn.innerHTML = "เพิ่มสินค้าลงตะกร้า";
+
+//         container.appendChild(containerItem);
+//         containerItem.appendChild(furniture_pic);
+//         containerItem.appendChild(containerDetail);
+//         containerDetail.appendChild(furniture_name);
+//         containerDetail.appendChild(size);
+//         containerDetail.appendChild(wood);
+//         containerDetail.appendChild(detailLable);
+//         containerDetail.appendChild(detail);
+//         containerDetail.appendChild(price);
+//         containerButton.appendChild(cartBtn);
+//         containerItem.appendChild(containerButton);
+
+//         showIDlayer.appendChild(container);
+
+//         document.getElementById(data[keys[i]].FID).onclick = getToCart;
+//     }
+// }
+
+async function getToCart(){
+    console.log(this.id);
+    writeCart(this.id);
+}
+
+async function writeCart(FID){
+    console.log("Add furniture to cart server");
+    const response = await fetch("/addDBcart", {
+        method: "POST",
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+        post:FID}) // ส่งค่า FID ไปให้ server.js
+    })
+    // const content = await response.json(); // ไม่ได้ใช้เนื่องจากไม่จำเป็นต้องเอาค่าที่ได้ไปทำอะไร
+    // console.log(content);
+    // showDataCart(content);
 }
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const hostname = 'localhost';
+const port = 3001;
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+var cookieParser = require('cookie-parser');
+var alert = require('alert');
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "HOLD_MY_CAT",
+    // socketPath: '/var/run/mysqld/mysqld.sock',
+    // port:"3001",
+    // multipleStatements: true
+})
+
+con.connect(err => {
+    if(err) throw(err);
+    else{
+        console.log("MySQL connected");
+    }
+})
+
+const queryDB = (sql) => {
+    return new Promise((resolve,reject) => {
+        // query method
+        con.query(sql, (err,result, fields) => {
+            if (err) reject(err);
+            else
+                resolve(result)
+        })
+    })
+}
+
+///////////////////// show all hotel /////////////////////
+
+app.get("/showDBChair", async (req,res) => {
+    let sql = `SELECT * FROM HOLD_MY_CAT.hotel_profile`;
+    let result = await queryDB(sql);
+    result = Object.assign({},result);
+    // console.log(result);
+    res.json(result);
+});
+
+///////////////////// show hotel detail /////////////////////
+
+app.post("/addDBcart", async (req,res) => {
+    let getCartID = req.body.post;
+    // console.log(getCartID);
+    let sql = `SELECT * FROM FROM HOLD_MY_CAT.hotel_profile WHERE hotel_id = ${getCartID}`;
+    let result = await queryDB(sql);
+    if(result.length > 0){
+        // sql = `SELECT quantity FROM OPEN_HOUSE_IDEA.cart WHERE furniture_id = ${getCartID} AND user_id = ${req.cookies.UID}`;
+        // result = await queryDB(sql);
+        // console.log(result);
+        sql = `UPDATE OPEN_HOUSE_IDEA.cart SET quantity= quantity + 1 WHERE furniture_id = ${getCartID} AND user_id = ${req.cookies.UID}`
+        result = await queryDB(sql);
+        sql = `SELECT quantity FROM OPEN_HOUSE_IDEA.cart WHERE furniture_id = ${getCartID} AND user_id = ${req.cookies.UID}`;
+        result = await queryDB(sql);
+        // console.log(result[0].quantity);
+        alert(`You are add ${result[0].quantity} furniture to cart`);
+        // alert('You are already add this furniture to cart');
+    }
+    else{
+        sql = `INSERT INTO OPEN_HOUSE_IDEA.cart (user_id, furniture_id, quantity) VALUES (${req.cookies.UID}, ${getCartID}, 1)`;
+        result = await queryDB(sql);
+        alert('Add this furniture to cart');
+    }
+});
