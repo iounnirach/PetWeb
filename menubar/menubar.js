0989a1b6bb08e647e1cdf25c132f6f9c219028b4
Onlypoom <wolfbox1027@outlook.com>
2021-05-30 20:30:25 +0700

    เสร็จ

--- menubar/menubar.js
+++
@@ -1,9 +0,0 @@
-const toggleButton  = document.getElementsByClassName('toggle-button')[0]
-const nav = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    nav.classList.toggle('active');
-})
-
-
-
